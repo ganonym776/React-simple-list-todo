a3463040cf9b7cd45400fab78f5cc09108dea0f8
gunawan.prasetya <gunawan.prasetya@bankraya.co.id>
2023-04-02 21:15:40 +0700

    Add component and logic

---
+++ src/model/model.ts
@@ -0,0 +1,34 @@
+
+export interface Todo {
+    id: number
+    todo: string
+    isDone: boolean
+}
+
+// type Action = 
+//   | {type:'add', payload: string}
+//   | {type:'remove', payload: number}
+//   | {type:'done', payload: number}
+
+
+// const TodoReducer = (state: Todo[], action: Action) => {
+//   switch (action.type) {
+//     case 'add' :
+//       return [
+//         ...state,
+//         { id:  Date.now(), todo: action.payload, isDone: false}
+//       ]
+//     case 'remove' :
+//       return state.filter((todo)=>todo.id !== action.payload)
+//     case 'done' :
+//       return state.map((todo)=> todo.id === action.payload ? {...todo, isDone: !todo.isDone} : todo)
+//   }
+// }
+
+// import { useReducer } from "react"
+// const ReducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, [])
+//   return (
+//     <div/>
+//   )
+// }
